453a17557ff2302eb8524b0d49aaea27c0b543e9
Elisha864 <dongmoelisha@gmail.com>
2026-03-17 08:34:34 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+    int a = 1;
+    int b = 5 ;
+    printf("Hello World \n");
+
+    if (a> b){
+        printf("a is greater than b \n");
+        printf("%d " ,a);
+    }
+    else {
+        printf("b is greater than a \n");
+        printf("%d" ,b);
+    }
+
+    return 0;
+
+}
